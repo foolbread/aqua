a6f1d566c8da2c690a916e22f944bbf64e04764f
foolbread <981848153@qq.com>
2015-12-21 10:55:28 +0800

    调整登陆服务器结构

--- login_server/config/config.go
+++ login_server/config/config.go
@@ -22,8 +22,11 @@ func loadConfig() {
 
 	str := con.MustString("server", "listen_addr", "")
 	golog.Info("listen_addr:", str)
-
 	g_config.setListenAddr(str)
+
+	strs := con.MustStringSlice("redis", "redis_addr", nil)
+	golog.Info("redis_addr:", strs)
+	g_config.setDBAddrs(strs)
 }
 
 func GetConfig() *loginServerConfig {
@@ -33,7 +36,8 @@ func GetConfig() *loginServerConfig {
 var g_config *loginServerConfig
 
 type loginServerConfig struct {
-	addr string
+	addr    string
+	db_addr []string
 }
 
 func (c *loginServerConfig) setListenAddr(a string) {
@@ -42,4 +46,12 @@ func (c *loginServerConfig) setListenAddr(a string) {
 
 func (c *loginServerConfig) GetListenAddr() string {
 	return c.addr
+}
+
+func (c *loginServerConfig) setDBAddrs(a []string) {
+	c.db_addr = a
+}
+
+func (c *loginServerConfig) GetDBAddrs() []string {
+	return c.db_addr
 }
--- login_server/server/server.go
+++ login_server/server/server.go
@@ -4,11 +4,7 @@
 package server
 
 import (
-	anet "aqua/common/net"
-	aproto "aqua/common/proto"
-	"aqua/login_server/config"
 	"fbcommon/golog"
-	"net"
 )
 
 func InitServer() {
@@ -18,70 +14,3 @@ func InitServer() {
 }
 
 var g_server *loginServer
-
-type loginServer struct {
-}
-
-func (s *loginServer) startListen() {
-	addr := config.GetConfig().GetListenAddr()
-
-	li, err := net.Listen("tcp", addr)
-	if err != nil {
-		golog.Critical(err)
-	}
-
-	golog.Info("start listen:", addr)
-
-	for {
-		c, err := li.Accept()
-		if err != nil {
-			golog.Critical(err)
-		}
-
-		go s.handleConnect(c)
-	}
-}
-
-func (s *loginServer) handleConnect(c net.Conn) {
-	//TO DO
-	var buf [1024]byte
-	defer c.Close()
-	//recv client data
-	l, err := anet.RecvPacket(c, buf[:])
-	if err != nil {
-		golog.Error(err)
-		return
-	}
-	//parse client data
-	h := aproto.UnmarshalHead(buf[:l])
-	switch h.Cmd {
-	case aproto.LOGINREQ_CMD:
-		req, err := aproto.UnmarshalLoginReq(buf[:l])
-		if err != nil {
-			golog.Error(err)
-			return
-		}
-		s.handleLogin(c, req)
-	}
-}
-
-func (s *loginServer) handleLogin(c net.Conn, req *aproto.LoginRequest) {
-	//check token
-
-	//get addr
-	var addr string
-	//get new token
-	var token []byte
-	//construct redirect
-	d, err := aproto.MarshalRedirect(token, addr)
-	if err != nil {
-		golog.Error(err)
-		return
-	}
-
-	err = anet.SendPacket(c, d)
-	if err != nil {
-		golog.Error(err)
-	}
-
-}
