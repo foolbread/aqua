f7d5ca831594042d227ef1dbd3b83f46eddce577
foolbread <981848153@qq.com>
2015-12-29 15:05:00 +0800

    no message

---
+++ login_server/server/connect_manager.go
@@ -0,0 +1,60 @@
+//@auther foolbread
+//@time 2015-12-29
+//@file aqua/login_server/server/connect_manager.go
+package server
+
+import (
+	"container/list"
+	"fbcommon/golog"
+	"sync"
+)
+
+type connectManager struct {
+	lo          *sync.RWMutex
+	connect_svr *list.List
+}
+
+func newConnectManager() *connectManager {
+	r := new(connectManager)
+	r.connect_svr = list.New()
+	r.lo = new(sync.RWMutex)
+
+	return r
+}
+
+func (s *connectManager) getConnectSvr(token []byte) *connectServer {
+	var ret *connectServer
+	s.lo.RLock()
+	idx := int(token[0]) % s.connect_svr.Len()
+	i := 0
+	for e := s.connect_svr.Front(); e != nil; e = e.Next() {
+		if i == idx {
+			ret = e.Value.(*connectServer)
+			break
+		}
+		i++
+	}
+	s.lo.RUnlock()
+
+	return ret
+}
+
+func (s *connectManager) addConnectSvr(svr *connectServer) {
+	s.lo.Lock()
+	s.connect_svr.PushBack(svr)
+	s.lo.Unlock()
+}
+
+func (s *connectManager) exitConnectSvr(Id uint32) {
+	s.lo.Lock()
+	for e := s.connect_svr.Front(); e != nil; e = e.Next() {
+		v := e.Value.(*connectServer)
+		if v.Id == Id {
+			golog.Info("Id:", Id, "addr:", v.Addr, "connect server exit...")
+			s.connect_svr.Remove(e)
+			break
+		}
+	}
+	s.lo.Unlock()
+
+}
---
+++ login_server/server/connect_server.go
@@ -0,0 +1,66 @@
+//@auther foolbread
+//@time 2015-12-29
+//@file aqua/login_server/server/connect_server.go
+package server
+
+import (
+	aerr "aqua/common/error"
+	anet "aqua/common/net"
+	aproto "aqua/common/proto"
+	fbatomic "fbcommon/atomic"
+	"fbcommon/golog"
+	"net"
+	"time"
+)
+
+var check_time time.Duration = 15 * time.Second
+var recv_time time.Duration = 30 * time.Second
+
+type connectServer struct {
+	Id    uint32
+	Addr  string
+	Alive fbatomic.AtomicBool
+	Con   net.Conn
+}
+
+func newConnectServer(id uint32, con net.Conn) *connectServer {
+	r := new(connectServer)
+	r.Id = id
+	r.Addr = con.RemoteAddr().String()
+	r.Con = con
+	r.Alive.Set(true)
+
+	return r
+}
+
+func (s *connectServer) run() {
+	var buf [1024]byte
+	var cmd uint32
+	var err error
+	login_timer.NewTimer(check_time, s.checkAlive)
+
+	for {
+		_, cmd, err = anet.RecvPacket(s.Con, buf[:])
+		if err != nil {
+			golog.Error(err)
+			break
+		}
+
+		if cmd != aproto.KEEPALIVE_CMD {
+			golog.Error(aerr.ErrKeepalive)
+			break
+		}
+
+		s.Alive.Set(true)
+	}
+}
+
+func (s *connectServer) checkAlive() {
+	if !s.Alive.Get() {
+		s.Con.Close()
+		return
+	}
+
+	s.Alive.Set(false)
+	login_timer.NewTimer(check_time, s.checkAlive)
+}
