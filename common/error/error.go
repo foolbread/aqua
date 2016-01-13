b52aff3cce3018bbed67749a38c5e908b6a44f85
foolbread <981848153@qq.com>
2016-01-13 11:11:21 +0800

    +添加新的错误

--- common/error/error.go
+++ common/error/error.go
@@ -8,10 +8,11 @@ import (
 )
 
 var (
-	ErrPacketLen  = errors.New("packet len is too long!")
-	ErrConnExsit  = errors.New("connect is exsit!")
-	ErrUnknowCmd  = errors.New("unknow cmd!")
-	ErrNoLogicSvr = errors.New("can't get any logic server!")
-	ErrKeepalive  = errors.New("keepalive error!")
-	ErrSession    = errors.New("invalid session!")
+	ErrPacketLen     = errors.New("packet len is too long!")
+	ErrConnExsit     = errors.New("connect is exsit!")
+	ErrUnknowCmd     = errors.New("unknow cmd!")
+	ErrNoLogicSvr    = errors.New("can't get any logic server!")
+	ErrKeepalive     = errors.New("keepalive error!")
+	ErrSession       = errors.New("invalid session!")
+	ErrSessionFormat = errors.New("session format error!")
 )
